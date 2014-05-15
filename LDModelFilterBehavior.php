deccd2d9d970039a6766f2680505fd864ccdcc0e
Louis DaPrato <l.daprato@gmail.com>
2014-05-15 18:58:50 -0500

    Added License

--- LDModelFilterBehavior.php
+++ LDModelFilterBehavior.php
@@ -1,8 +1,12 @@
 <?php
 /**
- * LDModelFilterBehavior class file
- * 
+ * LDModelFilterBehavior class file.
+ *
  * @author Louis A. DaPrato <l.daprato@gmail.com>
+ * @link https://lou-d.com
+ * @copyright 2014 Louis A. DaPrato
+ * @license The MIT License (MIT)
+ * @since 1.0
  */
 
 /**
